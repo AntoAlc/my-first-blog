66fbe98d40c5951c4290ffc30fac00789a9582ef
AntoAlc <antoo.alcaraz@gmail.com>
2018-08-04 15:35:58 -0300

    Mi app Django Girls, primer commit

---
+++ intro.py
@@ -0,0 +1,45 @@
+#if 5>2:	
+#	print ('5 is indeed greater than 2')
+#else:
+#	print('5 isnt greater than 2')
+
+
+
+#name='Anto'
+#if name== 'Sofi':
+#	print('Hey Sofi!')
+#elif name == 'Anto':
+#	print ('Hey Anto!')
+#else:
+#	print ('Hey anonymous!')
+
+#def hi():
+#	print('Hi there!')
+#	print('How are you?')
+#hi()
+
+
+#def hi(name):
+#	if name == 'Sofi':
+#		print('Hi Sofi!')
+#	elif name == 'Anto':
+#		print ('Hi Anto!')
+#	else:
+#		print ('Hi anonymous!')
+#hi('Maria')
+
+
+#def hi (name):
+#	print ('Hi ' + name + '!')
+#hi ("Rachel")
+
+
+#def hi (name):
+#	print ('Hi ' + name + '!')
+#girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+#for name in girls:
+#	hi (name)
+#	print ('next girl')
+
+for i in range (1,7):
+	print (i)
